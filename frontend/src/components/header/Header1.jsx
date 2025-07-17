fe89575e45357d245123cfd1dec4b596cfaeb35a
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-17 14:24:44 -0400

    Hot & typography

--- frontend/src/components/header/Header1.jsx
+++ frontend/src/components/header/Header1.jsx
@@ -8,53 +8,88 @@ const Header1 = () => {
     const colorMode = useContext(ColorModeContext);
     const theme = useTheme();
 
-   return (
-    <Box sx={{
-        bgcolor: "#2B3445",
-        py: "4px",
-        borderBottomRightRadius: 4,
-        borderBottomLeftRadius: 4,
-      }}
+    return (
+        <Box sx={{
+            bgcolor: "#2B3445",
+            py: "4px",
+            borderBottomRightRadius: 4,
+            borderBottomLeftRadius: 4,
+        }}
 
-
-
-
-
-
-
-
-            <div>
-                {theme.palette.mode === "light" ? (
-                    <IconButton
-                        onClick={() => {
-                            localStorage.setItem(
-                                "mode",
-                                theme.palette.mode === "dark" ? "light" : "dark"
-                            );
-                            colorMode.toggleColorMode();
+        >
+            <Container>
+                <Stack direction={"row"} alignItems={"center"}>
+                    <Typography
+                        sx={{
+                            mr: 2,
+                            p: "4px 10px",
+                            bgcolor: "#D23F57",
+                            borderRadius: "12px",
+                            fontSize: "12px",
+                            fontWeight: "bold",
+                            color: "#fff",
                         }}
-                        color="inherit"
+                        variant="body2"
                     >
-                        <LightModeOutlined />
-                    </IconButton>
-                ) : (
-                    <IconButton
-                        onClick={() => {
-                            localStorage.setItem(
-                                "mode",
-                                theme.palette.mode === "dark" ? "light" : "dark"
-                            );
-                            colorMode.toggleColorMode();
-                        }}
-                        color="inherit"
-                    >
-                        <DarkModeOutlined />
-                    </IconButton>
-                )}
-            </div>
+                        HOT
+                    </Typography>
 
-        </Box>
-    )
+                    <Typography
+                        sx={{
+                            fontSize: "12px",
+                            fontWeight: 300,
+                            color: "#fff",
+                        }}
+                        variant="body2"
+                    >
+                        Free Express Shipping
+                    </Typography>
+
+                    <Box flexGrow={1} />
+
+
+
+
+
+
+
+
+                    <div>
+                        {theme.palette.mode === "light" ? (
+                            <IconButton
+                                onClick={() => {
+                                    localStorage.setItem(
+                                        "mode",
+                                        theme.palette.mode === "dark" ? "light" : "dark"
+                                    );
+                                    colorMode.toggleColorMode();
+                                }}
+                                color="inherit"
+                            >
+                                <LightModeOutlined />
+                            </IconButton>
+                        ) : (
+                            <IconButton
+                                onClick={() => {
+                                    localStorage.setItem(
+                                        "mode",
+                                        theme.palette.mode === "dark" ? "light" : "dark"
+                                    );
+                                    colorMode.toggleColorMode();
+                                }}
+                                color="inherit"
+                            >
+                                <DarkModeOutlined />
+                            </IconButton>
+                        )}
+                    </div>
+           
+           
+           
+           
+           
+                </Box>
+                )
 }
 
-export default Header1;
+                export default Header1;
