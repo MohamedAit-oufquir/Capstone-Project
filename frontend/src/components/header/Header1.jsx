6654d21d18b5b7caafcebdd1b202c18577a1db3b
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-17 14:14:46 -0400

    mode&light

--- frontend/src/components/header/Header1.jsx
+++ frontend/src/components/header/Header1.jsx
@@ -1,43 +1,58 @@
-import React from 'react';
+import { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+import { IconButton, useTheme } from "@mui/material";
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+
 
 const Header1 = () => {
-  const colorMode = useContext(ColorModeContext);
-  const theme = useTheme();
-  
-    return(
-    <div>
+    const colorMode = useContext(ColorModeContext);
+    const theme = useTheme();
 
-        <div>
-            {theme.palette.mode === "light" ? (
-                <IconButton
-                    onClick={() => {
-                        localStorage.setItem(
-                            "mode",
-                            theme.palette.mode === "dark" ? "light" : "dark"
-                        );
-                        colorMode.toggleColorMode();
-                    }}
-                    color="inherit"
-                >
-                    <LightModeOutlinedIcon />
-                </IconButton>
-            ) : (
-                <IconButton
-                    onClick={() => {
-                        localStorage.setItem(
-                            "mode",
-                            theme.palette.mode === "dark" ? "light" : "dark"
-                        );
-                        colorMode.toggleColorMode();
-                    }}
-                    color="inherit"
-                >
-                    <DarkModeOutlinedIcon />
-                </IconButton>
-            )}
-        </div>
+    return (
+        <Box sx= {(
 
-    </div>
+            bgcolor:"#2B3445
+
+        )}>
+
+
+
+
+
+
+
+
+            <div>
+                {theme.palette.mode === "light" ? (
+                    <IconButton
+                        onClick={() => {
+                            localStorage.setItem(
+                                "mode",
+                                theme.palette.mode === "dark" ? "light" : "dark"
+                            );
+                            colorMode.toggleColorMode();
+                        }}
+                        color="inherit"
+                    >
+                        <LightModeOutlined />
+                    </IconButton>
+                ) : (
+                    <IconButton
+                        onClick={() => {
+                            localStorage.setItem(
+                                "mode",
+                                theme.palette.mode === "dark" ? "light" : "dark"
+                            );
+                            colorMode.toggleColorMode();
+                        }}
+                        color="inherit"
+                    >
+                        <DarkModeOutlined />
+                    </IconButton>
+                )}
+            </div>
+
+        </Box>
     )
 }
 
