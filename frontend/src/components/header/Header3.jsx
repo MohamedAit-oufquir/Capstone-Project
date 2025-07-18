a1daf4d41922d0692d1f38758ba0aae75f9b2c8c
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-17 23:24:43 -0400

    Create full-screen header menu with accordion submenus

--- frontend/src/components/header/Header3.jsx
+++ frontend/src/components/header/Header3.jsx
@@ -107,11 +107,84 @@ const Header3 = () => {
           <MenuIcon />
         </IconButton>
       )}
+      <Drawer
+        anchor={"top"}
+        open={state["top"]}
+        onClose={toggleDrawer("top", false)}
+        sx={{
+          ".MuiPaper-root.css-1sozasi-MuiPaper-root-MuiDrawer-paper": {
+            height: "100%",
+          },
+        }}
+      >
+        <Box
+          sx={{ width: 444, mx: "auto", mt: 6, position: "relative", pt: 10 }}
+        >
+          <IconButton
+            sx={{
+              ":hover": { color: "red", rotate: "180deg", transition: "0.3s" },
+              position: "absolute",
+              top: 0,
+              right: 10,
+            }}
+            onClick={toggleDrawer("top", false)}
+          >
+            <Close />
+          </IconButton>
 
+          {[
+            { mainLink: "Home", subLinks: ["Link 1", "Link 2", "Link 3"] },
+            { mainLink: "Mega menu", subLinks: ["Link 1", "Link 2", "Link 3"] },
+            {
+              mainLink: "full screen menu",
+              subLinks: ["Link 1", "Link 2", "Link 3"],
+            },
+            { mainLink: "pages", subLinks: ["Link 1", "Link 2", "Link 3"] },
+            {
+              mainLink: "user account",
+              subLinks: ["Link 1", "Link 2", "Link 3"],
+            },
+            {
+              mainLink: "vendor account",
+              subLinks: ["Link 1", "Link 2", "Link 3"],
+            },
+          ].map((item) => {
+            return (
+              <Accordion
+                key={item.mainLink}
+                elevation={0}
+                sx={{ bgcolor: "initial" }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>{item.mainLink}</Typography>
+                </AccordionSummary>
 
+                <List sx={{ py: 0, my: 0 }}>
+                  {item.subLinks.map((link) => {
+                    return (
+                      <ListItem key={link} sx={{ py: 0, my: 0 }}>
+                        <ListItemButton>
+                          <ListItemText primary={link} />
+                        </ListItemButton>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </Accordion>
+            );
+          })}
+        </Box>
+      </Drawer>
+    </Container>
+  );
+};
 
-</Container>        
-
-)
-}
 export default Header3;
+
+
+
+
