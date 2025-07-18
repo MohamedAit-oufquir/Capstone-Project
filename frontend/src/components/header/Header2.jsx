569f0e4d144245c75a3380c07fb721d7c058017a
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-17 20:57:59 -0400

    change commit

--- frontend/src/components/header/Header2.jsx
+++ frontend/src/components/header/Header2.jsx
@@ -1,12 +1,55 @@
+import { ExpandMore, ShoppingCartOutlined } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled } from "@mui/material/styles";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+const Search = styled("div")(({ theme }) => ({
+  flexGrow: 0.4,
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  border: "1px solid #777",
+  "&:hover": {
+    border: "1px solid #333",
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "266px",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "330px",
+  },
+}));
 
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "#777",
+}));
 
-const Header2 =()=>{
-    return(
-        <div>
-            Header 2
-        </div>
-    )
-}
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
 export default Header2;
 
 
