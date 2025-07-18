016475f756621b894a5b45222683ba8285824d10
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-17 21:11:06 -0400

    StyledBadge

--- frontend/src/components/header/Header2.jsx
+++ frontend/src/components/header/Header2.jsx
@@ -50,6 +50,15 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
   },
 }));
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+
+}));
 export default Header2;
 
 
