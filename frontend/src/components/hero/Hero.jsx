4b528240f658520c6539823b4ceab8c0b1b29866
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-18 10:34:53 -0400

    create a Hero folder& IconSection

---
+++ frontend/src/components/hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = ()=> {
+  return (
+    <container sx={{mt:2.5,display:"flex",alignItems:"center"}} >
+
+    <Box>
+
+        Slider
+
+    </Box>
+
+<Box>
+
+    Box 2
+
+</Box>
+
+    </container>
+  )
+}
+export default Hero;
