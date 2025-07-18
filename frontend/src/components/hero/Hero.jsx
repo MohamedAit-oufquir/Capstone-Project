6d714099e0c3e9c2454218c70100f9b946478f38
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-18 17:03:06 -0400

    Fix Slider

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,20 +1,105 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Link,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
-const Hero = ()=> {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "./slider.css";
+import IconSection from "./IconSection";
+
+const mySlider = [
+  { text: "MEN", link: "./images/banner-15.jpg" },
+  { text: "WOMEN", link: ".//images/banner-25.jpg" },
+];
+
+const Hero = () => {
+  const theme = useTheme();
   return (
-    <container sx={{mt:2.5,display:"flex",alignItems:"center"}} >
+    <Container>
+      <Box
+        sx={{ pt: 2, mt: 2.5, display: "flex", alignItems: "center", gap: 2 }}
+      >
+        <Swiper
+          loop={true}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {mySlider.map((item) => {
+            return (
+              <SwiperSlide key={item.link} className="parent-slider">
+                <img src={item.link} alt="" />
+                <Box
+                  sx={{
+                    [theme.breakpoints.up("sm")]: {
+                      position: "absolute",
+                      left: "10%",
+                      textAlign: "left",
+                    },
 
-    <Box>
+                    [theme.breakpoints.down("sm")]: {
+                      pt: 4,
+                      pb: 6,
+                    },
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      color: "#222",
+                    }}
+                    variant="h5"
+                  >
+                    LIFESTYLE COLLECTION
+                  </Typography>
 
-        Slider
+                  <Typography
+                    sx={{
+                      color: "#222",
+                      fontWeight: 500,
+                      my: 1,
+                    }}
+                    variant="h3"
+                  >
+                    {item.text}
+                  </Typography>
 
-    </Box>
-
-<Box>
-
-    Box 2
-
-</Box>
+                  <Stack
+                    sx={{
+                      justifyContent: { xs: "center", sm: "left" },
+                    }}
+                    direction={"row"}
+                    alignItems={"center"}
+                  >
+                    <Typography color={"#333"} mr={1} variant="h4">
+                      SALE UP TO
+                    </Typography>
+                    <Typography color={"#D23F57"} variant="h4">
+                      30% OFF
+                    </Typography>
+                  </Stack>
+                  <Typography
+                    sx={{
+                      color: "#000",
+                      fontWeight: 300,
+                      my: 1,
+                    }}
+                    variant="body1"
+                  >
+                    Get Free Shipping on orders over $99.00
+                  </Typography>
 
     </container>
   )
