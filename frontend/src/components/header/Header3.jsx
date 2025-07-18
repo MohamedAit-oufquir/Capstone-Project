ef2e3bc2dbc8b6cc505e24684b1b4391e6054fb0
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-07-17 22:45:13 -0400

    Designing the background of the website

--- frontend/src/components/header/Header3.jsx
+++ frontend/src/components/header/Header3.jsx
@@ -1,24 +1,45 @@
 import { Container } from "@muni/material";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import WindowIcon from "@mui/icons-material/Window";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import { useTheme } from "@mui/material";
 
 
 
-
-const Header3 = ()=> {
-    const [anchorEl, setAnchorEl]= useState(null);
+const Header3 = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
-    const handleClick = (event)=>{
+    const handleClick = (event) => {
         setAnchorEl(null);
     }
+
+    const theme = useTheme()
+
+
+
+
+
+
+
+
+
+
+
     return (
-        
         <Container>
             <Buttom
-
-                id="basic-button"
-                aria-controls={open ? "basic-menu" : undefined}
+             id="basic-button"
+                 aria-controls={open ? "basic-menu" : undefined}
                 aria-haspopup="true"
                 aria-expanded={open ? "true" : undefined}
                 onClick={handleClick}
+                sx={width:222,bgcolor:theme.palette.myColor.main,color:  theme.palette.text.secondary}
+
+
                 >
             </Buttom>
             <Menu
@@ -37,7 +58,7 @@ const Header3 = ()=> {
           </Menu>
 
 
-        </Container>
+        </Container >
     )
 
 
